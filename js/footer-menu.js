0e2060d06c9129afedf6fb2e21cfba4fd7a29cb4
twiddlethumbs <121984958+twiddlethumbs@users.noreply.github.com>
2023-05-14 21:12:35 -0500

    Update footer-menu.js
    changing the footer content

--- js/footer-menu.js
+++ js/footer-menu.js
@@ -2,12 +2,12 @@ function footernav() {
 	const element = document.getElementById("footernav");
 	var footer_begin = '<div class="footer_div">';
 	
-	var footer_text = '<div style="width:100%; margin-bottom:22px;">Top navigation customized from https://github.com/obscuredetour/simply-nav<div>';
+	var footer_text = '<div style="width:100%; margin-bottom:22px;">Top navigation<div>';
 	
-	var footer_links = '<a href="#" class="footer-link">test</a>';
+	var footer_links = '<a href="minimal-compute-page-generator.html" class="footer-link">Minimal Computing Page Maker</a>';
 	footer_links += '<a href="https://www.makeuseof.com/responsive-navigation-bar-using-html-and-css/" class="footer-link" target="_blank">Responsive Navigation Bar Tutorial</a>';
 	
 	var footer_end = '</div>';
 	
-	element.innerHTML = footer_begin + footer_text + footer_links + footer_end;
+	element.innerHTML = footer_begin + footer_links + footer_text + footer_end;
 }
