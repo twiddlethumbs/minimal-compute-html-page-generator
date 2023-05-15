ed21b09898be34ad4567c107ee59dff84b3a207e
twiddlethumbs <121984958+twiddlethumbs@users.noreply.github.com>
2023-05-14 21:24:05 -0500

    Update footer-menu.js
    Updates to the footer

--- js/footer-menu.js
+++ js/footer-menu.js
@@ -2,10 +2,10 @@ function footernav() {
 	const element = document.getElementById("footernav");
 	var footer_begin = '<div class="footer_div">';
 	
-	var footer_text = '<div style="width:100%; margin-bottom:22px;">Top navigation<div>';
-	
 	var footer_links = '<a href="minimal-compute-page-generator.html" class="footer-link">Minimal Computing Page Maker</a>';
 	footer_links += '<a href="https://www.makeuseof.com/responsive-navigation-bar-using-html-and-css/" class="footer-link" target="_blank">Responsive Navigation Bar Tutorial</a>';
+	
+	var footer_text = '<div style="width:100%; margin-top:22px;">This project grew from work in <div>';
 	
 	var footer_end = '</div>';
 	
